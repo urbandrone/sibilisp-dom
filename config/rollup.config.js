a00d25231e08b05e73f9373d2fe9b5f19b1c0cee
urbandrone <the.urban.drone@gmail.com>
2021-08-02 21:14:49 +0200

    Ready for first publish

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,13 +1,13 @@
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const buble = require('@rollup/plugin-buble');
-const terser = require('rollup-plugin-terser');
+const { terser } = require('rollup-plugin-terser');
 const filesize = require('rollup-plugin-filesize');
 
 module.exports = {
   input: './lib/index.mjs',
   output: {
     file: './lib/index.min.js',
-    type: 'umd',
+    format: 'umd',
     name: 'DOM'
   },
   plugins: [
